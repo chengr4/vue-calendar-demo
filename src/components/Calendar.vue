01c02f7f7ed026a1ff444c1b3f585bb10ddb2c95
Eddie Cheng <54136390+chengr4@users.noreply.github.com>
2021-03-28 00:12:38 +0800

    version 0.0.1

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,46 +1,49 @@
 <template>
   <v-row class="fill-height">
     <v-col>
-      <v-sheet height="60">
-        <v-toolbar flat>
-           <v-toolbar-title>
-            預約行事曆
-          </v-toolbar-title>
-          <v-btn outlined class="mx-4" @click="setToday"> Today </v-btn>
+      <v-sheet>
+        <v-toolbar flat id="toolbar">
+          <v-toolbar-title class="title"> 預約行事曆 </v-toolbar-title>
+          <v-btn outlined class="today mx-5" @click="setToday"> Today </v-btn>
           <v-btn fab text small @click="prev">
             <v-icon small> mdi-chevron-left </v-icon>
-          </v-btn>
-          <v-btn fab text small @click="next">
-            <v-icon small> mdi-chevron-right </v-icon>
           </v-btn>
           <v-toolbar-title v-if="$refs.calendar">
             {{ $refs.calendar.title }}
           </v-toolbar-title>
+          <v-btn fab text small @click="next">
+            <v-icon small> mdi-chevron-right </v-icon>
+          </v-btn>
           <v-spacer></v-spacer>
           <v-menu bottom right>
             <template v-slot:activator="{ on, attrs }">
-              <v-btn outlined rounded v-bind="attrs" v-on="on" color="#7f74b4">
+              <v-btn
+                rounded
+                outlined
+                v-bind="attrs"
+                v-on="on"
+                class="pick-date"
+              >
                 <span>{{ typeToLabel[type] }}</span>
                 <v-icon right> mdi-menu-down </v-icon>
               </v-btn>
             </template>
             <v-list>
               <v-list-item @click="type = 'day'">
-                <v-list-item-title>日檢視</v-list-item-title>
+                <v-list-item-title>日</v-list-item-title>
               </v-list-item>
               <v-list-item @click="type = 'week'">
-                <v-list-item-title>週檢視</v-list-item-title>
+                <v-list-item-title>週</v-list-item-title>
               </v-list-item>
-               <v-list-item @click="type = 'month'">
-                <v-list-item-title>月檢視</v-list-item-title>
+              <v-list-item @click="type = 'month'">
+                <v-list-item-title>月</v-list-item-title>
               </v-list-item>
             </v-list>
           </v-menu>
           <v-menu bottom right>
             <template v-slot:activator="{ on, attrs }">
-              <v-btn outlined rounded v-bind="attrs" v-on="on" color="#7f74b4">
+              <v-btn rounded outlined v-bind="attrs" v-on="on" class="member">
                 <span>{{ people[personIndex] }}</span>
-                <v-icon right> mdi-menu-down </v-icon>
               </v-btn>
             </template>
           </v-menu>
@@ -73,12 +76,12 @@
                 label="結束日期 (必填)"
               ></v-text-field>
               <v-text-field
-                v-model="start"
+                v-model="startTime"
                 type="time"
                 label="開始時間"
               ></v-text-field>
               <v-text-field
-                v-model="end"
+                v-model="endTime"
                 type="time"
                 label="結束時間"
               ></v-text-field>
@@ -95,11 +98,14 @@
         </v-card>
       </v-dialog>
 
-      <v-sheet height="85%">
+      <v-sheet height="80%">
         <v-calendar
           ref="calendar"
           v-model="focus"
           color="#7f74b4"
+          interval-width="40"
+          event-text-color="black"
+          :weekday-format="getDate"
           :events="events"
           :event-color="getEventColor"
           :type="type"
@@ -115,7 +121,7 @@
           :activator="selectedElement"
           offset-x
         >
-          <v-card color="grey lighten-4" min-width="350px" flat>
+          <v-card color="grey lighten-4" flat>
             <v-toolbar :color="selectedEvent.color" dark>
               <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
               <v-spacer></v-spacer>
@@ -150,12 +156,12 @@
             </v-card-actions>
           </v-card>
         </v-menu>
+        <v-container class="book">
+          <v-btn outlined rounded @click="dialog = true" class="btn-book">
+            + 新增預約
+          </v-btn>
+        </v-container>
       </v-sheet>
-      <v-container>
-        <v-btn outlined rounded block class="ma-auto" @click="dialog = true">
-          + 新增預約
-        </v-btn>
-      </v-container>
     </v-col>
   </v-row>
 </template>
@@ -164,24 +170,27 @@
 import { db } from "@/main";
 
 export default {
+  name: "Calendar",
   data: () => ({
     focus: "",
     type: "month",
     typeToLabel: {
-      month: "月檢視",
-      week: "週檢視",
-      day: "日檢視",
+      month: "月",
+      week: "週",
+      day: "日",
     },
     selectedEvent: {},
     selectedElement: null,
     selectedOpen: false,
     currentlyEditing: null,
     events: [],
-    people: ["James", "Karl", "Lily"],
+    people: ["莉"],
     personIndex: 0,
     dialog: false,
     start: null,
     end: null,
+    startTime: "00:00",
+    endTime: "00:00",
     details: null,
     name: null,
   }),
@@ -195,7 +204,7 @@ export default {
       this.type = "day";
     },
     getEventColor(event) {
-      event.color = "grey"
+      event.color = "grey";
       return event.color;
     },
     setToday() {
@@ -220,6 +229,7 @@ export default {
     showEvent({ nativeEvent, event }) {
       const open = () => {
         this.selectedEvent = event;
+        // <div>...
         this.selectedElement = nativeEvent.target;
         setTimeout(() => {
           this.selectedOpen = true;
@@ -231,11 +241,13 @@ export default {
       } else {
         open();
       }
-
       nativeEvent.stopPropagation();
     },
     async addEvent() {
       if (this.name && this.start && this.end) {
+        if (!this.checkDate()) return;
+
+        // 更新資料庫
         await db.collection("myEvent").add({
           name: this.name,
           details: this.details,
@@ -256,12 +268,79 @@ export default {
       await db.collection("myEvent").doc(ev).delete();
       (this.selectedOpen = false), this.getEvents();
     },
+    checkDate() {
+      const begin = new Date(`${this.start}` + `T${this.startTime}:00`);
+      const over = new Date(`${this.end}` + `T${this.endTime}:00`);
+
+      // 結束時間必須在開始時間之後
+      if (begin.getTime() <= over.getTime()) {
+        this.start = begin.toISOString().substr(0, 19);
+        this.end = over.toISOString().substr(0, 19);
+        return true;
+      } else {
+        alert("結束時間不可以早於開始時間");
+        return false;
+      }
+    },
+    // 更換星期顯示方式
+    getDate(date) {
+      const daysOfWeek = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"];
+      let i = date.weekday;
+      return daysOfWeek[i];
+    },
   },
 };
 </script>
 
 <style scoped>
-.ma-auto {
+/* 右上角的兩個按鈕 */
+.pick-date,
+.member {
+  background-color: #dedaf4;
+  height: 36px;
+  min-width: 64px;
+  margin: 0px 10px;
+}
+
+.btn-book {
   background-color: #7f74b4;
+  bottom: 5%;
+  position: fixed;
+  max-width: 200px;
+}
+
+.row {
+  max-width: 1200px;
+  max-height: 800px;
+  margin: auto;
+}
+
+.col {
+  display: flex-wrap;
+}
+
+/* Mobile (手機用) */
+@media (max-width: 500px) {
+  /* 右上角的兩個按鈕 */
+  .pick-date,
+  .member {
+    height: 36px;
+    min-width: 64px;
+    margin: 0px 2px;
+    background-color: #dedaf4;
+  }
+
+  .v-application .title,
+  .v-application .mx-5 {
+    display: none;
+  }
+  .col {
+    max-height: 660px;
+  }
+
+  /* toolbar 整體 */
+  .v-toolbar__content {
+    padding: 4px 0px;
+  }
 }
 </style>
